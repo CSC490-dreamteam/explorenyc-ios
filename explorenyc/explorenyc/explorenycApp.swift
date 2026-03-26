f43dffa2eb5b1251577836aa843e0bbe86ff8238
andrewkehoe <157737128+andrewkehoe@users.noreply.github.com>
2026-03-26 15:30:04 -0400

    Add initial Xcode SwiftUI app scaffold

---
+++ explorenyc/explorenyc/explorenycApp.swift
@@ -0,0 +1,17 @@
+//
+//  explorenycApp.swift
+//  explorenyc
+//
+//  Created by Andrew Kehoe on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct explorenycApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
